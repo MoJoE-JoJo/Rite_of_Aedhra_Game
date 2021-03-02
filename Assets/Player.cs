8456b19e72c59fbedf925f4ca588b93b972533dd
Andrea Trevisin <34127150+knowbot@users.noreply.github.com>
2021-03-02 02:14:00 +0100

    first camera demo complete

---
+++ Assets/Player.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Player : MonoBehaviour
+{
+
+    [SerializeField] private new Camera camera;
+    [SerializeField] private LayerMask pathableMask;
+    [SerializeField] private float lookAtSpeed = 10;
+    
+
+    private NavMeshAgent _agent;
+    private Animator _animator;
+    private bool _walking = false;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _agent = GetComponent<NavMeshAgent>();
+        _animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+            RaycastHit info;
+
+            if (Physics.Raycast(ray, out info, 100, pathableMask)) ;
+            {
+                _agent.SetDestination(info.point);
+            }
+        }
+        TurnToDestination();
+        _animator.Play(PathComplete() ? "Idle" : "Walking");
+    }
+    
+    protected bool PathComplete()
+    {
+        if ( _agent.remainingDistance <= _agent.stoppingDistance)
+        {
+            if (!_agent.hasPath || _agent.velocity.sqrMagnitude == 0f)
+            {
+                return true;
+            }
+        }
+ 
+        return false;
+    }
+
+    private void TurnToDestination()
+    {
+        if ((_agent.destination - _agent.transform.position).magnitude < 0.1f) return;
+
+        Vector3 direction = (_agent.destination - _agent.transform.position).normalized;
+        Quaternion qDir = Quaternion.LookRotation(direction);
+        _agent.transform.rotation = Quaternion.Slerp(_agent.transform.rotation, qDir, Time.deltaTime * lookAtSpeed);
+    }
+}
+
+
