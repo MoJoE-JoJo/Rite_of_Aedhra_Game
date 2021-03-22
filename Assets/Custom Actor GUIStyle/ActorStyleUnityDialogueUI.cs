fd345dcc235a8ed6f84c074d6d3bc71a4997fe08
Jonathan Emil Mogensen <jmog@itu.dk>
2021-03-22 18:08:48 +0100

    Added dialogue colors

---
+++ Assets/Custom Actor GUIStyle/ActorStyleUnityDialogueUI.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System;
+using PixelCrushers.DialogueSystem;
+using PixelCrushers.DialogueSystem.UnityGUI;
+
+public class ActorStyleUnityDialogueUI : UnityDialogueUI {
+
+	[Serializable]
+	public class ActorStyle {
+		public string actorName;
+		public string guiStyleName;
+	}
+
+	public ActorStyle[] actorStyles;
+
+	private string originalGuiStyleName = "label";
+
+	public override void Start() {
+		originalGuiStyleName = dialogue.npcSubtitle.line.guiStyleName;
+		base.Start();
+	}
+
+	public override void ShowSubtitle(Subtitle subtitle) {
+		dialogue.npcSubtitle.line.guiStyleName = originalGuiStyleName;
+		foreach (var actorStyle in actorStyles) {
+			if (string.Equals(subtitle.speakerInfo.Name, actorStyle.actorName)) {
+				dialogue.npcSubtitle.line.guiStyleName = actorStyle.guiStyleName;
+			}
+		}
+		base.ShowSubtitle(subtitle);
+	}
+
+}
