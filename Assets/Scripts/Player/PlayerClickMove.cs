16b10a8611d721109c339a8531efee6295877249
Andrea Trevisin <34127150+knowbot@users.noreply.github.com>
2021-05-17 18:44:14 +0200

    made some fixes to edge cases in the movement script

--- Assets/Scripts/Player/PlayerClickMove.cs
+++ Assets/Scripts/Player/PlayerClickMove.cs
@@ -128,15 +128,17 @@ namespace Player
         {
             Agent.ResetPath();
             _animator.SetBool(Walking, false);
+            _animator.SetBool(Running, false);
         }
 
         private void FaceDirection()
         {
-            if (Agent.velocity.sqrMagnitude == 0f) return;
-
+            if (PathComplete() || GameManager.Instance.PlayerController.IsDying) return;
             Vector3 direction = Agent.velocity.normalized;
             direction.y = 0;
+            if (direction == Vector3.zero) return;
             Quaternion qDir = Quaternion.LookRotation(direction);
+            if (qDir == Quaternion.identity) return;
             Agent.transform.rotation = Quaternion.Slerp(Agent.transform.rotation, qDir, Time.deltaTime * 10f);
         }
         
